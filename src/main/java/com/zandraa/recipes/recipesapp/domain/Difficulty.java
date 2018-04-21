db15602c6652abf7231adf4a301175b0019a34c4
Zandraa Attila Perjesi <zandraa.perjesi.ext@evosoft.com>
2018-04-21 13:09:43 +0200

    Add enum difficulty

---
+++ src/main/java/com/zandraa/recipes/recipesapp/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package com.zandraa.recipes.recipesapp.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
--- src/main/java/com/zandraa/recipes/recipesapp/domain/Recipe.java
+++ src/main/java/com/zandraa/recipes/recipesapp/domain/Recipe.java
@@ -18,6 +18,9 @@ public class Recipe {
     private String url;
     private String directions;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     Set<Ingredient> ingredients;
 
@@ -105,5 +108,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
