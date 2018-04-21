18802f592f268c84dd3b76c7d8a24438841e03f2
Zandraa Attila Perjesi <zandraa.perjesi.ext@evosoft.com>
2018-04-21 13:50:24 +0200

    add category

--- src/main/java/com/zandraa/recipes/recipesapp/domain/Recipe.java
+++ src/main/java/com/zandraa/recipes/recipesapp/domain/Recipe.java
@@ -22,7 +22,15 @@ public class Recipe {
     private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private
     Set<Ingredient> ingredients;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private
+    Set<Category> categories;
 
     @Lob
     private Byte[] image;
@@ -124,5 +132,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
