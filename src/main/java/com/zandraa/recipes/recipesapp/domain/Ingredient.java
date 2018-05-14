e9caaf4771c1ec331d7061fbde096c44fe7585b3
Zandraa Attila Perjesi <zandraa.perjesi.ext@evosoft.com>
2018-05-14 09:04:54 +0200

    Refactor

--- src/main/java/com/zandraa/recipes/recipesapp/domain/Ingredient.java
+++ src/main/java/com/zandraa/recipes/recipesapp/domain/Ingredient.java
@@ -18,14 +18,17 @@ public class Ingredient {
     @ManyToOne
     private Recipe recipe;
 
-    public Ingredient() {
-    }
-
     public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
         this.description = description;
         this.amount = amount;
         this.uom = uom;
         this.recipe = recipe;
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
     }
 
     public Long getId() {
--- src/main/java/com/zandraa/recipes/recipesapp/services/RecipeServiceImpl.java
+++ src/main/java/com/zandraa/recipes/recipesapp/services/RecipeServiceImpl.java
@@ -2,10 +2,12 @@ package com.zandraa.recipes.recipesapp.services;
 
 import com.zandraa.recipes.recipesapp.domain.Recipe;
 import com.zandraa.recipes.recipesapp.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
 public class RecipeServiceImpl implements RecipeService{
 
     private final RecipeRepository recipeRepository;
